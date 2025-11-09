a5839801cfd0ea1eb243aaa7c2be2957aa60071f
saket-bhadada <saketbhadada@gmail.com>
2025-11-09 23:31:21 +0530

    small editing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,23 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import Login from './login.jsx'
 import Registration from './registration.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/register",
-    element: <Registration />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Registration />
   </StrictMode>,
 )
