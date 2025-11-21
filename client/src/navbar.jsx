5ce00c33b6b03580c5aac233302a2a4abc1405ec
saket-bhadada <saketbhadada@gmail.com>
2025-11-21 17:41:43 +0530

    profile page editing

--- client/src/navbar.jsx
+++ client/src/navbar.jsx
@@ -4,6 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Link} from 'react-router-dom';
 
 function NavScrollExample() {
   return (
@@ -16,7 +17,7 @@ function NavScrollExample() {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link >Profile</Nav.Link>
+            <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
             <Nav.Link href="#action2"> </Nav.Link>
           </Nav>
           <Form className="d-flex">
