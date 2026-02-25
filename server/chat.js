d3099ad87aa79d064cedc4bdf1eb0595c9d16676
saket-bhadada <saketbhadada@gmail.com>
2026-02-25 16:22:34 +0530

    io

--- server/chat.js
+++ server/chat.js
@@ -3,7 +3,11 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 
-const app = express.Router();
-const server = http.createServer(app);
-
 const chat = express.Router();
+const server = http.createServer(chat);
+
+const io = new Server(server,{cors:{
+    origin:"http://localhost:3000/chat",
+    methods:["GET","POST"],
+    credentials:true
+}});
