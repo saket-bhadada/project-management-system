6defd0234f4cfe73ee4a2e6b6fe2f9250833f38c
saket-bhadada <saketbhadada@gmail.com>
2026-02-27 23:38:03 +0530

    http server enabling

--- server/chat.js
+++ server/chat.js
@@ -1,13 +1,5 @@
-import express from 'express';
-// import WebSocket,{ WebSocketServer } from 'ws';
 import http from 'http';
-import { Server } from 'socket.io';
+import WebSocketServer from 'ws';
 
-const chat = express.Router();
-const server = http.createServer(chat);
-
-const io = new Server(server,{cors:{
-    origin:"http://localhost:3000/chat",
-    methods:["GET","POST"],
-    credentials:true
-}});
+const server = http.createServer() //to take to http server
+const wsServer = new WebSocketServer({server})
