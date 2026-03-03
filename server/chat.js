0ca4d16bb77ec8eb16d91012e3a67fd037f7af82
saket-bhadada <saketbhadada@gmail.com>
2026-03-03 23:54:21 +0530

    use state updating

--- server/chat.js
+++ server/chat.js
@@ -1,6 +1,7 @@
 import { WebSocketServer } from "ws";
 import url from "url";
-import uuid from "uuid.v4";
+// import uuidv4 from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 export default function setupChat(server) {
   const wsServer = new WebSocketServer({
@@ -8,13 +9,23 @@ export default function setupChat(server) {
     path: "/chat",
   });
 
+  const connections = { };
+  const users = { };
+
     wsServer.on("connection", (connection,request) => {
         // const requestUrl = new URL(request.url, `http://${request.headers.host || 'localhost'}`);
         // const username = requestUrl.searchParams.get("username");
         const requestUrl = url.parse(request.url, true); // 'true' parses the query string into an object
         const username = requestUrl.query.username;
-        const uuidValue = uuid.v4();
-        console.log(`New connection from user: ${username} (UUID: ${uuidValue})`);
+        const uuid = uuidv4();
+        console.log(`New connection from user: ${username} (UUID: ${uuid})`);
+        connections[uuid] = connection;
+
+        users[uuid]={
+          username: username,
+          message:request.body?.message || "",
+          uuid:uuid
+        }
         // console.log("New connection from user:", username);
     }); 
 }
