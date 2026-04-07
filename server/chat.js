2be84f4c8a6fcc584abc1e30b5914af6fbd9cc27
saket-bhadada <saketbhadada@gmail.com>
2026-04-07 22:07:22 +0530

    chatting application update

--- server/chat.js
+++ server/chat.js
@@ -1,23 +1,31 @@
-import { WebSocketServer } from "ws";
-import http from "http";
+// import { WebSocketServer } from "ws";
+// import http from "http";
 
-export const httpserver = http.createServer((req,res)=>{
-  console.log("request received");
-})
-httpserver.listen(8080,()=>{
-  console.log("server started on port 8080");
-})
-export default function setupChat(server) {
-  // Use the same server instance and map the WebSocket to a specific path
-  const WebSocket = new WebSocketServer({
-      "httpServer":httpserver,
-      server,
-      path:"/chat"});
-  WebSocket.on("request",request=>{
-    connection = request.accept(null,request.origin);
-    console.log("request accepted");
-    connection.on("message",message=>{
-      console.log(`message : ${message}`);
-    })
-  })
+// export const httpserver = http.createServer((req,res)=>{
+//   console.log("request received");
+// })
+// httpserver.listen(8080,()=>{
+//   console.log("server started on port 8080");
+// })
+// export default function setupChat(server) {
+//   // Use the same server instance and map the WebSocket to a specific path
+//   const WebSocket = new WebSocketServer({
+//       "httpServer":httpserver,
+//       server,
+//       path:"/chat"});
+//   WebSocket.on("request",request=>{
+//     connection = request.accept(null,request.origin);
+//     console.log("request accepted");
+//     connection.on("message",message=>{
+//       console.log(`message : ${message}`);
+//     })
+//   })
+// }
+
+import express from "express";
+
+const chatRouter = express.Router();
+
+async function assertParticipant(userId,roomId,res) {
+  const {rows} = await db.query();
 }
