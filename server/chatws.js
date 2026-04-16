057115323b87b220d3eb15c5811f2b68dfae28ef
saket-bhadada <saketbhadada@gmail.com>
2026-04-16 12:17:00 +0530

    making server for websocket

--- server/chatws.js
+++ server/chatws.js
@@ -1,1 +1,12 @@
 import express from "express";
+import http from "http";
+
+const chatwsRoute = express.Router();
+
+const server = http.createServer(chatwsRoute);
+
+server.listen(8080,()=>{
+  console.log("server started on port 8080");
+})
+
+export default chatwsRoute;
