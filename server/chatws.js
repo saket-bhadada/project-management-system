5d42a417754956516f90ee1806c12178f0a237a6
saket-bhadada <saketbhadada@gmail.com>
2026-04-21 23:34:47 +0530

    changing the method of websocket file

--- server/chatws.js
+++ server/chatws.js
@@ -1,29 +1,8 @@
-import express from "express";
-import http from "http";
 import WebSocket from "ws";
-import db from "./db.js";
-import { chatRouter } from "./chat";
-import path from "path";
-const chatwsRoute = express.Router();
-const server = http.createServer(chatwsRoute);
+import passport from "passport";
+import db from "./db";
+import { addparticipantonAcceptance } from "./chat";
 
-chatwsRoute.use(express.json());
-
-const {Router:chatwsRouter} = require("./routes/chat");
-const acceptanceRouter = require("./routes/acceptance");
-
-chatwsRoute.use(",/api/chat",chatwsRouter);
-chatwsRoute.use("/api/acceptance",acceptanceRouter);
-
-const wss = new WebSocket.server({server,path:"/ws/chat"});
 const rooms = new Map();
 
-wss.on("connection",(ws,req)=>{
-  // const userId = 
-});
-
-server.listen(8080,()=>{
-  console.log("server started on port 8080");
-})
-
-export default chatwsRoute;
+export function setupChat(server) {}
