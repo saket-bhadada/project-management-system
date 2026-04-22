a9cf8b803d163ff16871337b65312aea33353fdb
saket-bhadada <saketbhadada@gmail.com>
2026-04-22 16:07:10 +0530

    making validation of user id

--- server/chatws.js
+++ server/chatws.js
@@ -5,4 +5,38 @@ import { addparticipantonAcceptance } from "./chat";
 
 const rooms = new Map();
 
-export function setupChat(server) {}
+export function setupChat(server,sessionParser) {
+  const wss = new WebSocket.server({
+    server,
+    path: "/chat",
+  });
+
+  wss.on("connection",(ws,req)=>{
+    sessionParser(req,{},()=>{
+      passport.initialize()(req,{},()=>{
+        passport.session()(req,{},async()=>{
+          if(!req.user){
+            console.warn("Unauthorized WebSocket connection attempt");
+            ws.send(JSON.stringify({type:"error",message:"Unauthorized"}));
+            ws.close();
+            return;
+          }
+          const userId = req.user.id;
+          let currentRoom = null;
+
+          ws.on("message",async(raw)=>{
+            let msg;
+            try{
+              msg = JSON.parse(raw);
+            }catch(e){
+              ws.send(JSON.stringify({type:"error",message:"Invalid message format"}));
+            }
+            if(msg.type === "join"){
+              
+            }
+          })
+        });
+      })
+    });
+  });
+}
