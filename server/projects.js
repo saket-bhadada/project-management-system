20d9315bf7173973dba3cdc513c49a9a1ab672e5
saket-bhadada <saketbhadada@gmail.com>
2026-05-08 23:27:09 +0530

    simple changes

--- server/projects.js
+++ server/projects.js
@@ -1,3 +1,9 @@
 import express from "express";
+import db from "./db.js";
 
 const ProjectRouter = express.Router();
+
+ProjectRouter.use((req,res,next)=>{
+    if(!req.isAuthenticated && req.isAuthenticated()) return next;
+    res.status(401).json({error:"Unauthorized"});
+})
