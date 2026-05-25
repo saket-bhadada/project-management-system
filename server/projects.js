c41d0c4d28a250ce94dccf25ef4a0413f2927dfd
saket-bhadada <saketbhadada@gmail.com>
2026-05-25 18:11:00 +0530

    posting any files feature added

--- server/projects.js
+++ server/projects.js
@@ -8,7 +8,7 @@ ProjectRouter.use((req, res, next) => {
   res.status(401).json({ error: "Unauthorized" });
 });
 
-ProjectRouter.get("/messages/files", async (req, res) => {
+ProjectRouter.get("/:messageId/files", async (req, res) => {
   const { messageId } = req.params;
   try {
     const access = await db.query(
@@ -36,4 +36,45 @@ ProjectRouter.get("/messages/files", async (req, res) => {
   }
 });
 
-ProjectRouter.get("/messages/files/:filesId", async (req, res) => {});
+ProjectRouter.get("/:messageId/files/:filesId", async (req, res) => {
+  const { messageId,filesId} = req.params;
+  try{
+    const access = await db.query(
+      `select 1 from chat_participants cp
+      join chat_rooms cr on cr.id = cp.room_id
+      where cp.user_id = $1 and cp.user_id = $2
+      union
+      select 1 from message where id = $1 and user_id = $2`,
+      [messageId, req.user.id]
+    );
+    if (!access.rows.length)
+      return res
+        .status(403)
+        .json({ message: "you do not have access to this file" });
+
+        const {rows} = await db.query(
+          `select pf.*,u.email as created_by_email
+          from project_files pf
+          join users u on u.id = pf.created_by
+          where pf.id = $1 and pf.message_id = $2`,
+          [filesId,messageId]
+        );
+
+        if(!rows.length) return res.status(404).json({message:"file not found"});
+        res.json({file:rows[0]});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+ProjectRouter.post("/:messageId/files",async(req,res)=>{
+  const {messageId} = req.params;
+  const {name,
+    path="/",
+    language = "plaintext",
+    content = ""
+  } = req.body;
+
+  if(!name) return res.status(400).json({message:"file name is required"});
+  try{}catch(error){}
+});
